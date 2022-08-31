6c86a082cb6a08b617cd9c69e94625be51db0e30
xImouto <mohammad.rafid.hamid@g.bracu.ac.bd>
2022-08-31 22:38:13 +0600

    added documentation

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# cordcommand
+# Cordcommand
+
+## About
+A utility package for making discord-bot commands much easier to write with discord.js.
+
+## Usage Example
+
+```js
+// initiate discord.js client
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+// define your commands similar to
+const commands: MessageCommand[] = [
+  {
+    regex: /ping/i,
+    message: "pong",
+  },
+  {
+    regex: /hi/i,
+    // message can also be a callback function which can access the discord message object
+    message: (_, message) => `hello <@${message.author.id}>`,
+  },
+  {
+    regex: /bye/i,
+    message: (_, message) => `bye ${message.author.username}`,
+    reply: true, // uses discord's message.reply intead of just sending the message in the same channel
+  },
+  {
+    regex: /args/i,
+    message: (args) => `The arguments are: ${args.join(", ")}`,
+    reply: true, 
+  },
+];
+
+// add commands to the client by calling the addCommands function provided by the client
+addCommands(client, commands, {
+  messageCommandPrefix: /^i!/i,
+  // Add your command prefix regex. Make sure to include ^ (starts with) in the regex
+});
+
+// login
+client.login(process.env.BOT_TOKEN!);
+```
--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,70 @@ import { Client, Message } from "discord.js";
 
 export const allMatch = /.*/;
 
+/**
+ * Function to help send message in discord
+ * @param messageArgs message arguments string array returned by MessageArgumentParser. Default parser excludes the message command prefix
+ * @param message The discord Message object. Accessing it allows using author, channel etc. of the message
+ * @returns string message to send in discord
+ */
 export type MessageArgCb = (messageArgs: string[], message: Message) => string;
 
+/**
+ * Function to do anything you want to instead of just sending a message
+ * @param message The discord Message object. Accessing it allows using author, channel etc. of the message
+ */
+export type MessageCb = (message?: Message) => void;
+
+/**
+ * Function to parse arguments incase the default one is not according to expectations
+ * @param messageContent the string message content to process into arguments
+ * @returns should return arguments in the form of an array of string
+ */
+export type MessageArgumentParserCb = (messageContent: string) => string[];
+
+/**
+ * Object explaining what a command does
+ * @property regex - the regex used to match with the message content detect the command
+ * @property cb - A function to give users complete control with what to do with the message object
+ * @property message - a message to send or funtion returning a message to send
+ * @property reply - boolean indicating whether to send the message by replying to the command message or not
+ */
 export interface MessageCommand {
   regex: RegExp;
-  cb?: (message?: Message) => void;
-  message?: string | MessageArgCb; // post message without replying
+  cb?: MessageCb;
+  message?: string | MessageArgCb;
   reply?: boolean;
 }
 
 export interface AddCommandsOptions {
   messageCommandPrefix: RegExp;
-  messageCommandArgumentParser?: (messageContent: string) => string[];
+  messageArgumentParser?: (messageContent: string) => string[];
 }
 
-export function messageCommandArgumentParser(messageContent: string, messageCommandPrefix: RegExp | string) {
+/**
+ * The default function to parse arguments 
+ * @param messageContent the string message content to process into arguments
+ * @param messageCommandPrefix the regex command prefix to not pass it into the arguments array
+ * @returns arguments in the form of an array of string
+ */
+export function messageArgumentParser(messageContent: string, messageCommandPrefix: RegExp | string) {
   return messageContent.toLocaleLowerCase().trim().replace(/\s\s+/g, " ").replace(messageCommandPrefix, "").split(" ");
 }
 
+/**
+ * Main function to add commands to the discord bot client
+ * @param client The discord client to "add" commands on
+ * @param commands Array of objects, each representing the information of a command
+ * @param options Optional. Needed for passing command prefix regex and/or message argument parser
+ */
 export function addCommands(client: Client, commands: MessageCommand[], options?: AddCommandsOptions) {
   client.on("messageCreate", async (message) => {
     if (!message.content.match(options?.messageCommandPrefix || allMatch)) {
       return;
     }
-    const args = options?.messageCommandArgumentParser
-      ? options.messageCommandArgumentParser(message.content)
-      : messageCommandArgumentParser(message.content, options?.messageCommandPrefix || "");
+    const args = options?.messageArgumentParser
+      ? options.messageArgumentParser(message.content)
+      : messageArgumentParser(message.content, options?.messageCommandPrefix || "");
     for (let command of commands) {
       if (!message.content.match(command.regex)) {
         continue;
@@ -39,7 +77,19 @@ export function addCommands(client: Client, commands: MessageCommand[], options?
   });
 }
 
-function sendMessage(message: string | MessageArgCb, discordMessage: Message, messageArgs: string[], reply?: boolean) {
+/**
+ * Helper function which is needed for addCommands to work. Not intended to be used by consumers. Used to send message to discord.
+ * @param message message in string to send, or function that returns a message in string to send
+ * @param discordMessage The discord Message object. Accessing it allows using author, channel etc. of the message
+ * @param messageArgs The arguments of the message. May be needed by the message function
+ * @param reply boolean to indicate whether or not to reply to the message while sending it
+ */
+export function sendMessage(
+  message: string | MessageArgCb,
+  discordMessage: Message,
+  messageArgs: string[],
+  reply?: boolean
+) {
   if (typeof message === "string") reply ? discordMessage.reply(message) : discordMessage.channel.send(message);
   else
     reply
